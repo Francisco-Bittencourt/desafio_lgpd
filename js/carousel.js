80b994b78ff49d4c6b586727af2adac2df70c186
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-05-23 11:55:44 -0300

    alt2

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,5 @@
-// Armazena os slides
 let carouselArr = [];
 
-// Classe Carousel
 class Carousel {
     constructor(image, title, url) {
         this.image = image;
@@ -9,32 +7,19 @@ class Carousel {
         this.url = url;
     }
 
-    static _sequence = 0;
-    static _size = 0;
-    static _interval = null;
+    static index = 0;
+    static setInterval = 0;
 
-    // Inicia o carrossel
-    static Start(arr) {
-        if (arr && arr.length > 0) {
-            Carousel._sequence = 0;
-            Carousel._size = arr.length;
-            Carousel.Show(arr[Carousel._sequence]); // mostra o primeiro slide
-            Carousel._interval = setInterval(function () {
-                Carousel.Next(arr);
-            }, 5000);
-        } else {
-            throw "Method Start needs a non-empty array.";
-        }
+    static Start(carouselArray) {
+        Carousel.Show(carouselArray[Carousel.index]);
+
+        Carousel.setInterval = setInterval(() => {
+            Carousel.index = (Carousel.index + 1) % carouselArray.length;
+            Carousel.Show(carouselArray[Carousel.index]);
+        }, 5000);
     }
 
-    // Avança para o próximo slide
-    static Next(arr) {
-        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
-        Carousel.Show(arr[Carousel._sequence]);
-    }
-
-    // Mostra o slide atual
-    static Show(carouselItem) {
+        static Show(carouselItem) {
         const carouselDiv = document.getElementById("carousel");
         const titleDiv = document.getElementById("carousel-title");
 
@@ -42,3 +27,4 @@ class Carousel {
         titleDiv.innerHTML = `<a href="${carouselItem.url}">${carouselItem.title}</a>`;
     }
 }
+
